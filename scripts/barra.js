2b5edbea4c123bb9227e3e9517c7c62d38028ae7
Carlos Montes <cdmontes@uninorte.edu.co>
2021-06-04 11:32:28 -0500

    Mostrar y ocultar barra lateral del mapa

---
+++ scripts/barra.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "515px";
+    document.getElementById("main").style.marginLeft = "515px";
+}
+ 
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
