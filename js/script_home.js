2401f9033f4648e2ea8df9b741c0a2fcbed08d1d
adanielm <adanielm@uninorte.edu.co>
2021-06-05 11:47:32 -0500

    Acciones en la vista de home

---
+++ js/script_home.js
@@ -0,0 +1,15 @@
+function Sabor(){
+    location.href = "https://saborbarranquilla.com";
+}
+
+function Auto(){
+    location.href = "https://autofest.co/";
+}
+
+function Agro(){
+    location.href = "https://agroexpocaribe.com/";
+}
+
+function Carnaval(){
+    location.href = "https://www.barranquilla.gov.co/descubre/carnaval-de-barranquilla";
+}
