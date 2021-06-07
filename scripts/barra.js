dbe99553d63a7d4f0d1368e09bc0e63c910bd15d
Carlos Montes <cdmontes@uninorte.edu.co>
2021-06-06 23:05:25 -0500

    Quitar barra desplegable innecesaria

--- scripts/barra.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("sideNavigation").style.width = "515px";
-    document.getElementById("main").style.marginLeft = "515px";
-}
- 
-function closeNav() {
-    document.getElementById("sideNavigation").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
